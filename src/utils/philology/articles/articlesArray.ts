1a91ed6dc99eec80281ea999d4629c376b7ce2e0
Maria Morales <maremarismaria@gmail.com>
2023-08-04 17:01:50 +0100

    :bug: Consume PDFs from public folder

--- src/utils/philology/articles/articlesArray.ts
+++ src/utils/philology/articles/articlesArray.ts
@@ -1,12 +1,11 @@
 import type { Article } from "@/types";
 import { articlesDictionary as articles } from "@/utils/philology/articles/articlesDictionary";
 
-const baseURL = "https://mariamorales.wiki";
-const slug = "articles/philology";
+const slug = "/articles/philology";
 
 export default [
   {
-    href: `${baseURL}/${slug}/${articles["Ramon_Gomez_de_la_Serna_Greguerias_radio_y_el_cine"]}`,
+    href: `${slug}/${articles["Ramon_Gomez_de_la_Serna_Greguerias_radio_y_el_cine"]}`,
     title: "Ramón Gómez de la Serna: Greguerías y otros divertimentos en la radio y el cine",
     tags: [
       "TFM",
@@ -19,7 +18,7 @@ export default [
     ],
   },
   {
-    href: `${baseURL}/${slug}/${articles["La_tradicion_clasica_en_De_Institutione_Feminae_Christianae"]}`,
+    href: `${slug}/${articles["La_tradicion_clasica_en_De_Institutione_Feminae_Christianae"]}`,
     title:
       "La tradición clásica en 'De Institutione Feminae Christianae' de Juan Luis Vives. Estudio de las figuras femeninas mitológico-literarias",
     tags: [
