5025b3cb155ba46c4d1c2e16464a5d787c814628
Maria Morales <maremarismaria@gmail.com>
2023-08-05 12:27:26 +0100

    :card_file_box: Add articles

--- src/db/utils/philology/articles/articlesArray.ts
+++ src/db/utils/philology/articles/articlesArray.ts
@@ -5,10 +5,24 @@ const slug = "/articles/philology";
 
 export default [
   {
+    href: "#",
+    title: "WIP",
+    tags: [
+      "Ph.D.",
+      "Philology",
+      "Applied Linguistics",
+      "Greece",
+      "Latin",
+      "Greek",
+      "Inheritance",
+      "Foundations"
+    ],
+  },
+  {
     href: `${slug}/${articles["Ramon_Gomez_de_la_Serna_Greguerias_radio_y_el_cine"]}`,
     title: "Ramón Gómez de la Serna: Greguerías y otros divertimentos en la radio y el cine",
     tags: [
-      "TFM",
+      "Final Master's Project (TFM)",
       "Contemporary Literature",
       "Ramón Gómez de la Serna",
       "Greguerías",
@@ -22,14 +36,80 @@ export default [
     title:
       "La tradición clásica en 'De Institutione Feminae Christianae' de Juan Luis Vives. Estudio de las figuras femeninas mitológico-literarias",
     tags: [
-      "TFG",
+      "Bachelor's Thesis (TFG)",
       "Juan Luis Vives",
       "Renaissance Literature",
       "Latin",
       "Greek",
+      "Greece",
       "Women",
       "Mithology",
       "Conduct books",
     ],
+  },
+  {
+    href: `${slug}/${articles["El_discurso_performativo_de_Ramon_Gomez_de_la_Serna_una_aproximacion"]}`,
+    title: "El discurso performativo de Ramón Gómez de la Serna: una aproximación",
+    tags: [
+      "Essay",
+      "Ramón Gómez de la Serna",
+      "Ramonismo",
+      "Radio",
+      "Film",
+      "Spanish Language",
+      "Theatre",
+      "Avant-Garde",
+    ],
+  },
+  {
+    href: `${slug}/${articles["Oscar_Wilde_y_una_muerte_sin_importancia"]}`,
+    title:
+      "'Oscar Wilde y una muerte sin importancia' de Gyles Brandreth: novela de detectives",
+    tags: [
+      "Essay",
+      "Oscar Wilde",
+      "Gyles Brandreth",
+      "Novela de detectives",
+    ],
+  },
+  {
+    href: `${slug}/${articles["El_tiempo_entre_costuras"]}`,
+    title:
+      "'El tiempo entre costuras' como producto cultural: claves de un éxito literario y audiovisual",
+    tags: [
+      "Essay",
+      "Culture",
+      "Product",
+      "TV Show",
+      "Spanish Literature"
+    ],
+  },
+  {
+    href: `${slug}/${articles["El_metacine_en_la_película_El_extra"]}`,
+    title:
+      "El metacine en la película 'El extra' de Miguel M. Delgado",
+    tags: [
+      "Essay",
+      "Mexican Culture",
+      "Miguel M. Delgado",
+      "Film",
+      "Theatre",
+      "Inheritance",
+    ],
+  },
+  {
+    href: `${slug}/${articles["Epistola_Cadaver_Anotada"]}`,
+    title: "[Anotada] Epístola Cádaver",
+    tags: ["Writings", "Avant-Garde", "Spanish Literature"],
+  },
+  {
+    href: articles["Epistola_Cadaver"],
+    title: "Epístola Cádaver",
+    tags: ["Writings", "Avant-Garde", "Spanish Literature"],
+  },
+  {
+    href: articles["Greguerias"],
+    title: "Greguerías",
+    tags: ["Writings", "Greguería", "Ramón Gómez de la Serna"],
   },
 ] as Article[];
